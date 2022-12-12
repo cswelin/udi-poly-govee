f41e982205f098fae04a98cd8912fd9378bbccc2
Colin Swelin <colin@swelin.com>
2022-12-12 10:18:03 -0500

    Template

---
+++ govee-poly.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import udi_interface
+import sys
+
+LOGGER = udi_interface.LOGGER
+
+from nodes import Controller
+
+if __name__ == "__main__":
+    try:
+        polyglot = udi_interface.Interface([Controller, GooveNode])
+        polyglot.start()
+        control = Controller(polyglot, 'controller', 'controller', 'Govee Controller')
+        polyglot.runForever()
+    except (KeyboardInterrupt, SystemExit):
+        LOGGER.warning("Received interrupt or exit...")
+        """
+        Catch SIGTERM or Control-C and exit cleanly.
+        """
+        polyglot.stop()
+    except Exception as err:
+        LOGGER.error('Excption: {0}'.format(err), exc_info=True)
+    sys.exit(0)
---
+++ nodes/__init__.py
@@ -0,0 +1,5 @@
+
+""" Node classes used by the Python template Node Server. """
+
+from .GooveNode       import GooveNode
+from .Controller      import Controller
